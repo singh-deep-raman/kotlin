b23d77037984a82be4b2ed3823bb00281ac7e3b8
RamandeepSingh <raman111092@gmail.com>
2025-11-14 00:19:54 +0530

    first kotlin program

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kotlin
 Hands on Coding learning of Kotlin
+
+Courses:
+
+1. https://www.udemy.com/course/kotlinmasterclass/?couponCode=MT251110G2
+Complete Kotlin Development Masterclass 
---
+++ udemy/complete-kotlin-development-masterclass/src/lecture1/Main.kt
@@ -0,0 +1,7 @@
+package lecture1
+
+// first kotlin program
+// every program needs a run configuration to run, generally we specify main class in the run configuration in IDE
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
