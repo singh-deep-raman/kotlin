cd7cc335c4272a053b1cc384cfc2f66a6dc0b334
RamandeepSingh <raman111092@gmail.com>
2025-11-15 13:35:34 +0530

    Variables and Strings small assignments

---
+++ udemy/complete-kotlin-development-masterclass/src/lecture3/VariablesAndStringsPractice.kt
@@ -0,0 +1,23 @@
+package lecture3
+
+fun main(args: Array<String>) {
+    // assignment 1
+    val someInfo = "My cat's name is \"Fluffy\""
+    println(someInfo)
+
+    // assignment 2
+    val anotherInfo = "Two type of slashes: \\ and /"
+    println(anotherInfo)
+
+    // assignment 3
+    val googleDomain = "http://www.google.com"
+    println(googleDomain)
+
+    // assignment 4
+    val line1 = "(\\(\\"
+    val line2 = "(-.-)"
+    val line3 = "o_(\")(\")"
+    println(line1)
+    println(line2)
+    println(line3)
+}
