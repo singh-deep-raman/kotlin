59a7eabe1e3c0243ec8a1d9c3074150a24a8ee46
RamandeepSingh <raman111092@gmail.com>
2025-11-19 12:00:53 +0530

    String templates contains expressions

---
+++ udemy/complete-kotlin-development-masterclass/src/lecture5/StringTemplates.kt
@@ -0,0 +1,10 @@
+package lecture5
+
+fun main(args: Array<String>) {
+    // Strings in kotlin can contain expressions
+    val dogName = "Bruno"
+    println("My name is $dogName")
+
+    // e.g. arithmetic expressions in strings
+    println("I have ${1+1} cats")
+}
